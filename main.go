fe9a81f55b42373ff5896f1bb9b13b35195354ea
Umut ÇELİK <umutc@users.noreply.github.com>
2021-11-06 20:26:53 +0300

    implement a connection to the mysql

--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type UserTitle struct {
+	UserID    uint32
+	TitleID   uint8
+	YearID    uint8
+	IsDefault bool
+}
+
 func main() {
-	message, err := Hello("Master")
+	// create connection to the mysql database and execute query. dont forget imports
+	db, err := sql.Open("mysql", "root:1002@tcp(localhost:3306)/app")
 	if err != nil {
-		print(err.Error())
-		return
+		panic(err.Error())
 	}
 
-	print(message)
+	defer db.Close()
+
+	// query the database
+	results, err := db.Query("SELECT user_id, title_id, year_id, is_default FROM user_title WHERE year_id = 10")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// print results
+	for results.Next() {
+		// scan the result and assign to UserTitle
+		var userTitle UserTitle
+		err = results.Scan(&userTitle.UserID, &userTitle.TitleID, &userTitle.YearID, &userTitle.IsDefault)
+		if err != nil {
+			panic(err.Error())
+		}
+		// print the result
+		// fmt.Print(userTitle)
+	}
+
+	// close the mysql database connection
+	defer db.Close()
 }
